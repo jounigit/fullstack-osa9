206aa3301f9fbe4b0040eef98ddc0eb7652db0d5
Jouni <airaksinen.jouni@gmail.com>
2020-04-01 20:41:42 +0300

    9.6 Eslint

--- exerciseCalculator.ts
+++ exerciseCalculator.ts
@@ -1,30 +1,30 @@
 
 interface ExerciseInfo {
-    periodLength: number,
-    trainingDays: number,
-    success: boolean,
-    rating: number,
-    ratingDescription: string,
-    target: number,
-    average: number
+    periodLength: number;
+    trainingDays: number;
+    success: boolean;
+    rating: number;
+    ratingDescription: string;
+    target: number;
+    average: number;
 }
 
 const parseArgs = (args: Array<string>) => {
     args.forEach( arg => {
         if (isNaN(Number(arg))) {
-            throw new Error('Provided values were not numbers!')
+            throw new Error('Provided values were not numbers!');
         }
-    })
-    return args.map( a => Number(a) )
-}
+    });
+    return args.map( a => Number(a) );
+};
 
 const calculateExercises = (args: Array<number>): ExerciseInfo => {
-    const trainingData = args.slice(1)
-    const periodLength: number = trainingData.length
-    const sum: number = trainingData.reduce((x, y) => x + y)
-    const targetHours: number = periodLength * args[0]
-    const average: number = sum / periodLength
-    const rating: number = sum >= targetHours ? 3 : sum >= (targetHours/2) ? 2 : 1
+    const trainingData = args.slice(1);
+    const periodLength: number = trainingData.length;
+    const sum: number = trainingData.reduce((x, y) => x + y);
+    const targetHours: number = periodLength * args[0];
+    const average: number = sum / periodLength;
+    const rating: number = sum >= targetHours ? 3 : sum >= (targetHours/2) ? 2 : 1;
 
     return {
         periodLength: periodLength,
@@ -34,12 +34,12 @@ const calculateExercises = (args: Array<number>): ExerciseInfo => {
         ratingDescription: rating === 3 ? 'excellent job!!!' : rating === 2 ? 'not too bad but could be better' : 'lousy job',
         target: args[0],
         average: average
-    }
-}
+    };
+};
 
 try {
-    const argsNumber = parseArgs( process.argv.slice(2) )
-    console.log(calculateExercises(argsNumber))
+    const argsNumber = parseArgs( process.argv.slice(2) );
+    console.log(calculateExercises(argsNumber));
   } catch (e) {
     console.log('Error, something bad happened, message: ', e.message);
   }
