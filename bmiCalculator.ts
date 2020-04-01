8af84b73e02fe46fa28e783ddc224e1e2d34891a
Jouni <airaksinen.jouni@gmail.com>
2020-04-01 09:35:03 +0300

    9.3 Command line

--- bmiCalculator.ts
+++ bmiCalculator.ts
@@ -1,17 +1,34 @@
 
-const calculateBmi = (pituus: number, paino: number) => {
-    let kerroin: number = 0.01
-
-    pituus = pituus * kerroin
-    const bmi = paino / ( pituus * pituus ) 
-    // console.log(bmi)
-
-    if( 19 <= bmi && bmi <= 25) {
-        return 'Normal (healthy weight)'
-    } else {
-        return 'Underweight or overweight (unhealthy weight)'
-    }
-    
+interface CalculateValues {
+    value1: number;
+    value2: number;
 }
 
-console.log(calculateBmi(180, 74))
+const parseArguments = (args: Array<string>): CalculateValues => {
+    if (args.length < 4) throw new Error('Not enough arguments');
+    if (args.length > 4) throw new Error('Too many arguments');
+  
+    if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+      return {
+        value1: Number(args[2]),
+        value2: Number(args[3])
+      }
+    } else {
+      throw new Error('Provided values were not numbers!');
+    }
+  }
+
+const calculateBmi = (pituus: number, paino: number) => {
+    pituus = pituus * 0.01
+    const bmi = paino / ( pituus * pituus ) 
+    // console.log(bmi)
+    let result = 25 <= bmi ? 'Overweight' : 19 <= bmi ? 'Normal (healthy weight)' : 'Underweight'
+    console.log(result) 
+}
+
+try {
+    const { value1, value2 } = parseArguments(process.argv);
+    calculateBmi(value1, value2);
+  } catch (e) {
+    console.log('Error, something bad happened, message: ', e.message);
+  }
--- exerciseCalculator.ts
+++ exerciseCalculator.ts
@@ -1,3 +1,4 @@
+
 interface ExerciseInfo {
     periodLength: number,
     trainingDays: number,
@@ -8,31 +9,37 @@ interface ExerciseInfo {
     average: number
 }
 
-const calculateExercises = (args: Array<number>, targetPerDay: number): ExerciseInfo => {
-    const periodLength: number = args.length
-    const sum: number = args.reduce((x, y) => x + y)
-    const targetHours: number = periodLength * targetPerDay
+const parseArgs = (args: Array<string>) => {
+    args.forEach( arg => {
+        if (isNaN(Number(arg))) {
+            throw new Error('Provided values were not numbers!')
+        }
+    })
+    return args.map( a => Number(a) )
+}
+
+const calculateExercises = (args: Array<number>): ExerciseInfo => {
+    const trainingData = args.slice(1)
+    const periodLength: number = trainingData.length
+    const sum: number = trainingData.reduce((x, y) => x + y)
+    const targetHours: number = periodLength * args[0]
     const average: number = sum / periodLength
     const rating: number = sum >= targetHours ? 3 : sum >= (targetHours/2) ? 2 : 1
 
     return {
         periodLength: periodLength,
-        trainingDays: args.filter( a => a !== 0 ).length,
+        trainingDays: trainingData.filter( a => a !== 0 ).length,
         success: sum === targetHours,
         rating: rating,
         ratingDescription: rating === 3 ? 'excellent job!!!' : rating === 2 ? 'not too bad but could be better' : 'lousy job',
-        target: targetPerDay,
+        target: args[0],
         average: average
     }
 }
 
-
-let arr: number[] = [3, 0, 2, 4.5, 0, 3, 1]
-// let arrB: number[] = [3, 0, 4.5, 0, 1]
-// let arrC: number[] = [3, 0, 0, 0, 1, 1]
-// let arrD: number[] = [3, 2, 3,5, 2, 1,5, 3]
-
-console.log( calculateExercises(arr, 2) )
-console.log('==EX B==', calculateExercises(arrB, 2) )
-console.log('==EX C==', calculateExercises(arrC, 2) )
-console.log('==EX D==', calculateExercises(arrD, 2) )
+try {
+    const argsNumber = parseArgs( process.argv.slice(2) )
+    console.log(calculateExercises(argsNumber))
+  } catch (e) {
+    console.log('Error, something bad happened, message: ', e.message);
+  }
