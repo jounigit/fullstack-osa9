b6375349528d3b8007a70f4549ced0da3aa11930
Jouni <airaksinen.jouni@gmail.com>
2020-04-06 09:00:05 +0300

    9.13: Patientor backend, step6

--- src/utils.ts
+++ src/utils.ts
@@ -1,24 +1,49 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { NewPatientEntry } from './types';
+import { NewPatientEntry, Gender } from './types';
 
+//*********** parse string ******************/ 
 const isString = (text: any): text is string => {
     return typeof text === 'string' || text instanceof String;
-  };
+};
 
-const parseString = (stringToParse: any): string => {
-    if (!stringToParse || !isString(stringToParse)) {
-      throw new Error('Incorrect or missing comment: ' + stringToParse);
-    }
-  
-    return stringToParse;
-  };
+const parseString = (param: any): string => {
+  if (!param || !isString(param)) {
+    throw new Error('Incorrect or missing input: ' + param);
+  }  
+  return param;
+};
+
+
+//*********** parse date ******************/
+const isDate = (date: string): boolean => {
+  return Boolean(Date.parse(date));
+};
+
+const parseDate = (date: any): string => {
+  if (!date || !isString(date) || !isDate(date)) {
+      throw new Error('Incorrect or missing date: ' + date);
+  }
+  return date;
+};
+
+//*********** parse date ******************/
+const isGender = (param: any): param is Gender => {
+  return Object.values(Gender).includes(param);
+};
+
+const parseGender = (gender: any): Gender => {
+  if (!gender || !isGender(gender)) {
+    throw new Error('Incorrect or missing gender: ' + gender);
+  }
+  return gender;
+};
 
 const toNewPatientEntry = (object: { name: any; dateOfBirth: any; ssn: any; gender: any; occupation: any }): NewPatientEntry => {
   const newEntry: NewPatientEntry = {
         name: parseString(object.name),
-        dateOfBirth: parseString(object.dateOfBirth),
+        dateOfBirth: parseDate(object.dateOfBirth),
         ssn: parseString(object.ssn),
-        gender: parseString(object.gender),
+        gender: parseGender(object.gender),
         occupation: parseString(object.occupation),
     };
 
