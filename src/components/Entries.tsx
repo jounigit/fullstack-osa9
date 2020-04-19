7c4b61996275e4e026e360d586517c455176c6b4
Jouni <airaksinen.jouni@gmail.com>
2020-04-19 23:07:30 +0300

    9.22: patientor, step7

--- src/components/Entries.tsx
+++ src/components/Entries.tsx
@@ -1,31 +1,73 @@
 import React, { Fragment } from "react";
-import { Entry } from '../types';
-import { useStateValue } from "../state";
+import { Entry,
+    OccupationalHealthcareEntry,
+    HealthCheckEntry,
+    HospitalEntry,
+} from '../types';
+// import { useStateValue } from "../state";
+import { Segment, Header, Icon } from "semantic-ui-react";
 
-const EntryContent = ({entry}: {entry: Entry}) => {
-    const [{ diagnosis }, ] = useStateValue();
+const Hospital: React.FC<{entry: HospitalEntry}> = ({entry}) => {
+    // const healtColorList = {0:'green', 1: 'yellow', 2: 'orange', 3: 'red'};
+    // const color = ;
     return (
-        <>
-        <p> {entry.date} {entry.description} </p>
-        <ul>
-            {
-                entry.diagnosisCodes?.map(( code, i ) => 
-            <li key={i}>{code} {diagnosis[code].name}</li>
-                )
-            }                    
-        </ul>
-        </>
+        <Segment>
+            <Header as='h3'>
+                <Header.Content>
+                    {entry.date} <Icon name='hospital' />
+                    <Header.Subheader>{entry.description}</Header.Subheader>
+                </Header.Content>
+            </Header>
+            <Header as='h4'>
+                <Header.Content>
+                Discharge
+                </Header.Content>
+                <Header.Subheader>{entry.discharge?.date} {entry.discharge?.criteria}</Header.Subheader>
+            </Header>
+        </Segment>
     );
 };
 
-const ShowEntry = ({entry}: {entry: Entry}) => {
+const getColor = (rateNumber: number) => {
+    return rateNumber === 0 ? "green" : 
+    rateNumber === 1 ? "yellow" : rateNumber === 2 ? "orange" : "red";
+};
+
+const HealthCheck: React.FC<{entry: HealthCheckEntry}> = ({entry}) => {
+    return (
+        <Segment>
+            <Header as='h3'>
+                <Header.Content>
+                    {entry.date} <Icon name='user md' />
+                    <Header.Subheader>{entry.description}</Header.Subheader>
+                    <Icon name='heart' color={getColor(entry.healthCheckRating)} /> 
+                </Header.Content>
+            </Header>
+        </Segment>
+    );
+};
+
+const OccupationalHealthcare: React.FC<{entry: OccupationalHealthcareEntry}> = ({entry}) => {
+    return (
+        <Segment>
+            <Header as='h3'>
+                <Header.Content>
+                    {entry.date} <Icon name='stethoscope' /> {entry.employerName}                    
+                    <Header.Subheader>{entry.description}</Header.Subheader>
+                </Header.Content>
+            </Header>
+        </Segment>
+    );
+};
+
+const EntryDetails: React.FC<{entry: Entry}> = ({entry}) => {
     switch (entry.type) {
         case "HealthCheck":
-            return <EntryContent entry={entry} />;
+            return <HealthCheck entry={entry} />;
         case "Hospital":            
-            return <EntryContent entry={entry} />;   
+            return <Hospital entry={entry} />;   
         case "OccupationalHealthcare":           
-            return <EntryContent entry={entry} />;
+            return <OccupationalHealthcare entry={entry} />;
     }
 };
 
@@ -36,7 +78,7 @@ export const Entries = ({entries}: {entries: Entry[]}) => {
                 entries.map(( val, index ) => {
                     return (
                         <div key={index}>
-                            <ShowEntry entry={ val } />
+                            <EntryDetails entry={ val } />
                         </div>
                     );
                 })
